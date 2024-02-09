0d5e07da7ffda50571d68fdeb261f602fa346476
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-02-09 12:27:49 +0300

    names fix add task8

---
+++ task8.php
@@ -0,0 +1,17 @@
+<?php
+
+$dateVar = '01-02-2024';
+$date = new DateTime($dateVar);
+$endDate = new DateTime($dateVar);
+
+$endDate->modify('+1 month');
+
+while ($date < $endDate) {
+    if ($date->format('N') < 6) {
+        echo $date->format('d-m-Y') . "</br>";
+    } elseif ($date->format('N') == 7) {
+        $date->modify('+1 day');
+        echo $date->format('d-m-Y') . "</br>"; 
+    }
+    $date->modify('+3 days');
+}
